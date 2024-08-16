20bccbdc96d68417c3fddf6fb4ff007e402b36f8
Muhammad Usman <140269245+Muhammad-Usman28@users.noreply.github.com>
2024-08-16 23:34:03 +0500

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// config.js
-module.exports = {
-    mongoURI: 'mongodb+srv://usmanirfan460:802977@cluster1.aw8p3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1' // Make sure MongoDB is running on this URI
-};
